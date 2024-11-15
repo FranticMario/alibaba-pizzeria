fa9cf7c21bf8b2e0fc08f74682cd318687bcb5d6
Andrej Dortmann <andrej.dortmann25@gmail.com>
2024-11-15 14:08:00 +0100

    feat: delete photo section

--- src/menu.js
+++ src/menu.js
@@ -1,33 +1,37 @@
 const menu = {
     "Kebab Gerichte (Drehspieß)": [
-        { id: 1, name: "Kebaptasche / Pita", ingredients: "gem. Salat, Krautsalat & Tzatziki", price: 6.00 },
-        { id: 2, name: "Kebaptasche / Pita und Weißkäse", ingredients: "gem. Salat, Krautsalat, Tzatziki, Zwiebeln & Weißkäse", price: 7.00 },
-        { id: 3, name: "Vegetarische Tasche", ingredients: "Weißkäse, gemischter Salat, Tzatziki & Zwiebeln", price: 6.00 },
-        { id: 4, name: "Kebab Teller", ingredients: "Weißkäse, gem. Salat, Pommes & Tzatziki", price: 11.00 },
-        { id: 5, name: "Kebab Dürüm", ingredients: "Brot mit Kebapfleisch, Salat & Tzatziki (gerollt)", price: 7.00 },
-        { id: 6, name: "Vegetarischer Dürüm", ingredients: "Fladenbrot mit gemischtem Salat, Tzatziki, Weißkäse & Krautsalat (gerollt)", price: 7.00 },
-        { id: 7, name: "Pom Kebab Dürüm", ingredients: "Brot mit Kebapfleisch, Pommes, Salat & Tzatziki (gerollt)", price: 8.00 },
-        { id: 8, name: "Kleiner Kebab Teller", ingredients: "Pommes, Salat & Tzatziki", price: 8.00 },
-        { id: 9, name: "Kebab Box", ingredients: "Pommes oder Salat & Sauce", price: 7.00 },
-        { id: 10, name: "Sucuk Tasche", ingredients: "Salat, Krautsalat & Tzatziki", price: 8.00 }
+        { id: 1, name: "Kebaptasche / Pita", ingredients: "gem. Salat, Krautsalat & Tzatziki", price: 6.50 },
+        { id: 2, name: "Kebaptasche / Pita und Weißkäse", ingredients: "gem. Salat, Krautsalat, Zwiebeln & Weißkäse", price: 7.50 },
+        { id: 3, name: "Vegetarische Tasche", ingredients: "Weißkäse, gemischter Salat, Tzatziki & Zwiebeln", price: 6.50 },
+        { id: 4, name: "Kebab Teller", ingredients: "Weißkäse, gem. Salat, Pommes & Tzatziki", price: 11.50 },
+        { id: 5, name: "Kebab Dürüm", ingredients: "Brot mit Kebapfleisch, Salat & Tzatziki (gerollt)", price: 7.50 },
+        { id: 6, name: "Vegetarischer Dürüm", ingredients: "Fladenbrot mit gemischtem Salat, Tzatziki, Weißkäse & Krautsalat (gerollt)", price: 7.50 },
+        { id: 7, name: "Pom Kebab Dürüm", ingredients: "Brot mit Kebapfleisch, Pommes, Salat & Tzatziki (gerollt)", price: 8.50 },
+        { id: 8, name: "Kleiner Kebab Teller", ingredients: "Pommes, Salat & Tzatziki", price: 8.50 },
+        { id: 9, name: "Kebab Box", ingredients: "Pommes oder Salat & Sauce", price: 7.50 },
+        { id: 10, name: "Sucuk Tasche", ingredients: "Salat, Krautsalat & Tzatziki", price: 8.50 }
     ],
     "Hähnchen Döner Gerichte": [
-        { id: 12, name: "Hähnchentasche / Pita", ingredients: "gem. Salat, Krautsalat & Tzatziki", price: 7.00 },
-        { id: 13, name: "Hähnchentasche / Pita & Weißkäse", ingredients: "gem. Salat, Krautsalat, Tzatziki, Zwiebeln & Weißkäse", price: 8.00 },
-        { id: 14, name: "Hähnchen Teller", ingredients: "Salat, Tzatziki & Pommes", price: 11.00 },
-        { id: 15, name: "Hähnchen Dürüm", ingredients: "Brot mit Hähnchenfleisch, Salat & Tzatziki (gerollt)", price: 7.00 },
+        { id: 12, name: "Hähnchentasche / Pita", ingredients: "gem. Salat, Krautsalat & Tzatziki", price: 6.50 },
+        { id: 13, name: "Hähnchentasche / Pita & Weißkäse", ingredients: "gem. Salat, Krautsalat, Zwiebeln & Weißkäse", price: 7.50 },
+        { id: 14, name: "Hähnchen Teller", ingredients: "Salat, Tzatziki & Pommes", price: 11.50 },
+        { id: 15, name: "Hähnchen Dürüm", ingredients: "Brot mit Hähnchenfleisch, Salat & Tzatziki (gerollt)", price: 7.50 },
         { id: 16, name: "Kleiner Hähnchen Döner Teller", ingredients: "Pommes, Tzatziki & Salat", price: 8.50 },
-        { id: 17, name: "Hähnchen Box", ingredients: "Pommes oder Salat & Sauce", price: 7.00 },
-        { id: 18, name: "Pom Hähnchen Dürüm", ingredients: "Brot mit Hähnchenfleisch, Pommes & Tzatziki (gerollt)", price: 8.50 }
+        { id: 17, name: "Hähnchen Box", ingredients: "Pommes oder Salat & Sauce", price: 7.50 },
+        { id: 18, name: "Pom Hähnchen Dürüm", ingredients: "Brot mit Hähnchenfleisch, Pommes & Tzatziki (gerollt)", price: 8.50 },
+        { id: 19, name: "Großer Hähnchen Teller", ingredients: "Pommes, Tzatziki & Salat", price: 11.50 },
+        { id: 20, name: "Hähnchen Tasche/Pita", ingredients: "mit Gemüse, Sauce und Hähnchenfleisch", price: 8.50 }
     ],
-    "Lahmacun (Türkische Pizza) & Pide": [
-        { id: 210, name: "Lahmacun", ingredients: "gefüllt aus unserer Salattheke, Tzatziki & Weißkäse", price: 6.00 },
-        { id: 211, name: "Lahmacun", ingredients: "Kebab- oder Hähnchenfleisch, Salat & Tzatziki", price: 7.50 },
-        { id: 212, name: "Lahmacun XXL", ingredients: "Kebab- oder Hähnchenfleisch, Salat & Tzatziki", price: 11.00 },
-        { id: 213, name: "Peynirli Pide", ingredients: "frisch aufgerollter Teig mit Weißkäse überbacken", price: 8.50 },
-        { id: 214, name: "Sucuklu Pide", ingredients: "frisch aufgerollter Teig mit türk. Knoblauchwurst & Gouda überbacken", price: 9.00 },
-        { id: 215, name: "Kiymali Pide", ingredients: "frisch aufgerollter Teig mit Hackfleisch", price: 9.50 }
+    "Burger": [
+        { id: 160, name: "Hamburger", ingredients: "180g der Klassiker", price: 6.50 },
+        { id: 161, name: "Hamburger XXL", ingredients: "180g der Klassiker", price: 8.50 },
+        { id: 162, name: "Cheeseburger", ingredients: "180g mit Schmelzkäse überbacken", price: 7.50 },
+        { id: 163, name: "Cheeseburger XXL", ingredients: "180g mit Schmelzkäse überbacken", price: 9.50 },
+        { id: 164, name: "Baconburger", ingredients: "180g mit Schmelzkäse und knusprigem Bacon", price: 8.00 },
+        { id: 165, name: "Jalapeno Burger", ingredients: "180g mit Rindfleisch, Käse und pikanten Jalapenos", price: 10.00 },
+        { id: 166, name: "Chickenburger", ingredients: "mit zarter Hähnchenbrust, mit Schmelzkäse überbacken", price: 10.00 }
     ],
+
     "Überbacken (mit Sahne, Tomatensauce & Käse)": [
         { id: 20, name: "Kebab (Drehspieß) Überbacken", ingredients: "", price: 9.00 },
         { id: 21, name: "Kebab (Drehspieß) Überbacken", ingredients: "mit Pilzen & Zwiebeln", price: 10.00 },
